8fd9d80e7003e29ab813d08b890c703d31b07cf3
Ramiro <ramirocanario1@gmail.com>
2022-05-09 18:04:14 -0300

    contar de lineas y caracteres agregado

--- main.hs
+++ main.hs
@@ -21,3 +21,11 @@ quitar _ [] = []
 quitar x (cabeza:resto) | x == cabeza = resto
 quitar x (y:resto) = y: quitar x resto
 
+-- contar: cuenta cosas, que pueden ser caracteres, palabras o líneas. 
+-- La función debe ser realizada de manera de poder recibir la opción de que contar como una función argumento
+contar_chars palabra = length(map (:[]) palabra)
+
+contar "lineas" lista = length lista
+
+contar "caracteres" [] = 0
+contar "caracteres" (cabeza:resto) = contar_chars cabeza + contar "caracteres" resto
