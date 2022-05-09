b5eec5d7f3759266d6dd09048ff28692c95086e0
Ramiro <ramirocanario1@gmail.com>
2022-05-09 17:19:01 -0300

    funciones rev y quitar agregadas

--- main.hs
+++ main.hs
@@ -1,3 +1,23 @@
+-- UTILS:
+
+-- funcion que invierte una cadena
+invertir :: [a] -> [a]  
+invertir = foldl (\acc x -> x : acc) []
+
+
+
+-- RESOLUCION:
+
 -- tac: revierte el orden de líneas de un archivo, es decir, devuelve el archivo en el orden inverso
 tac [] = []
 tac (x:xs) = tac xs ++ [x]
+
+-- rev: revierte las cadenas que componen el archivo
+rev [] = []
+rev (x) = map invertir x
+
+-- quitar: quita la ocurrencia de una línea dada.
+quitar _ [] = []
+quitar x (cabeza:resto) | x == cabeza = resto
+quitar x (y:resto) = y: quitar x resto
+
